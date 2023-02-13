937e3c79c443ae26ed05dee8f04f630e192e75bf
banniroy <matinadekola33@gmail.com>
2023-02-13 13:10:17 +0100

    new

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react()],
-+  base: '/banniroy/.github.io/react-todo-list_gh'
+
 })
